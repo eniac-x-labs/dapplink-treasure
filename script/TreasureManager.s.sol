446ab6645f92b17120c227af43d4478e72716188
seek <guoshijiang2012@163.com>
2024-08-20 11:35:14 +0800

    update grant script (#3)

--- script/TreasureManager.s.sol
+++ script/TreasureManager.s.sol
@@ -1,21 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script} from "forge-std/Script.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
+import {Script, console } from "forge-std/Script.sol";
 import "../src/TreasureManager.sol";
-import "../src/access/proxy/Proxy.sol";
 
 contract TreasureManagerScript is Script {
-    TreasureManager public treasureManager;
-    Proxy public proxyTreasureManager;
+    ProxyAdmin public dappLinkProxyAdmin;
+    TreasureManager public treasureManagerContract;
 
     function run() public {
-        vm.startBroadcast();
-        address admin = msg.sender;
+        // env
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address usdtTokenAddress =  vm.envAddress("USDT_ADDRESS");
+        address withdrawManager =  vm.envAddress("WITHDRAW_MANAGER");
+        address treasureManager =  vm.envAddress("TREASURE_MANAGER");
 
-        treasureManager = new TreasureManager();
-        proxyTreasureManager = new Proxy(address(treasureManager), admin, "");
-        treasureManager.initialize(admin, admin);
+        // deployerAddress
+        address deployerAddress = vm.addr(deployerPrivateKey);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        dappLinkProxyAdmin = new ProxyAdmin(deployerAddress);
+        console.log("deploy dappLinkProxyAdmin:", address(dappLinkProxyAdmin));
+
+        treasureManagerContract = new TreasureManager();
+
+        TransparentUpgradeableProxy proxyTreasureManager = new TransparentUpgradeableProxy(
+            address(treasureManagerContract),
+            address(dappLinkProxyAdmin),
+            abi.encodeWithSelector(TreasureManager.initialize.selector, treasureManager, withdrawManager)
+        );
+        console.log("deploy proxyTreasureManager:", address(proxyTreasureManager));
+
+        // setup
+        ITreasureManager(address(proxyTreasureManager)).setTokenWhiteList(usdtTokenAddress);
 
         vm.stopBroadcast();
     }
